698986a8510e7c04170596b6cdef96dce4b99a1a
xnoroor <Crolonnlol@gmail.com>
2025-03-14 11:22:04 +0700

    3rd commit

--- js/script.js
+++ js/script.js
@@ -1,21 +1,26 @@
-//navbar-fixed
-window.onscroll = function(){
+// Navbar Fixed
+window.onscroll = function () {
     const header = document.querySelector('header');
     const fixednav = header.offsetTop;
 
-    if(window.pageYOffset > fixednav) {
+    if (window.pageYOffset > fixednav) {
         header.classList.add('navbar-fixed');
-    }else{
+    } else {
         header.classList.remove('navbar-fixed');
     }
-}
+};
 
-
-//Burger
+// Burger Menu Toggle with Animation
 const Burger = document.querySelector('#burger');
 const navmenu = document.querySelector('#nav-menu');
 
-Burger.addEventListener('click', function(){
+Burger.addEventListener('click', function () {
     Burger.classList.toggle('burger-Active');
-    navmenu.classList.toggle('hidden'); 
+    navmenu.classList.toggle('hidden');
+    
+    if (!navmenu.classList.contains('hidden')) {
+        navmenu.classList.add('animate__fadeInDown');
+    } else {
+        navmenu.classList.remove('animate__fadeInDown');
+    }
 });
